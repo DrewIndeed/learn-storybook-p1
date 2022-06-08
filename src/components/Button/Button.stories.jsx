aa69eafceb59a340fda4a4a7c5f9f4db1c8469fa
Andrew Le <s3820098@rmit.edu.vn>
2022-06-08 20:55:12 +0700

    7. Args mechanism

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -10,3 +10,27 @@ export const Primary = () => <Button variant="primary">Primary</Button>;
 export const Secondary = () => <Button variant="secondary">Secondary</Button>;
 export const Success = () => <Button variant="success">Success</Button>;
 export const Danger = () => <Button variant="danger">Danger</Button>;
+
+const Template = (args) => <Button {...args} />;
+
+export const PrimaryA = Template.bind({});
+PrimaryA.args = {
+  variant: "primary",
+  children: "Primary Args",
+  args: {
+    // args to apply to all individual stories
+    children: "The Boss Args",
+  },
+};
+
+export const LongPrimaryA = Template.bind({});
+LongPrimaryA.args = {
+  ...PrimaryA.args, // inherit props from other named export
+  children: "Long Primary Args", // if not specified, this is "The Boss Args"
+};
+
+export const SecondaryA = Template.bind({});
+SecondaryA.args = {
+  variant: "secondary",
+  children: "Secondary Args",
+};
